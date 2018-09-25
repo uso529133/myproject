6cbe1f41cb073c940d2f813ccdb9e1c8321f8849
uso529133 <uso529133@gmail.com>
2018-09-25 15:52:30 +0900

    9/25 #4

--- Main.cpp
+++ Main.cpp
@@ -1,31 +1,64 @@
 
 #include "Tetris.h"
 #include "TetrisFactory.h"
+#include <conio.h>
 
 const int width = 15;
 const int height = 20;
-	
+
+enum Key { Up = 72, Down = 80, Left = 75, Right = 77 };
+
 int main() {
 
 	TetrisFactory factory;
+	int input;
 	
 	Tetris* game = factory.MakeTetris(width, height);
-	Block* block = factory.MakeBlock(1, width / 2 - 2);
+	Block* block = factory.MakeBlock(width);
 	
 	while (true) {
 		block->MoveBy(1, 0);
 		if (game->isDuplicateWith(block)) {
 			block->MoveBy(-1, 0);
 			game->ApplyBlock(block);
-			block = factory.MakeBlock(1, width / 2 - 2);
+			block = factory.MakeBlock(width);
+			if (game->isDuplicateWith(block)) {
+				printf("Game Over!\n");
+				break;
+			}
 		}
-
+		
+		if (kbhit()) {
+			int input = getch();
+			if (input == 224) {
+				switch (getch()) {
+					case Key::Up:
+						block->Rotate();
+						break;
+					case Key::Down:
+						block->MoveBy(1, 0);
+						if (game->isDuplicateWith(block)) { block->MoveBy(-1, 0); }
+						break;
+					case Key::Left:
+						block->MoveBy(0, -1);
+						if (game->isDuplicateWith(block)) { block->MoveBy(0, 1); }
+						break;
+					case Key::Right:
+						block->MoveBy(0, 1);
+						if (game->isDuplicateWith(block)) { block->MoveBy(0, -1); }
+						break;
+				}
+			}
+		}
+		
+		
 		game->RefreshBuffer(block);
 		game->PrintBuffer();
 		
 		_sleep(100);
 	}
 	
+	getchar();
 	
 	return 0;
 }
--- TetrisFactory.h
+++ TetrisFactory.h
@@ -1,17 +1,22 @@
 
+#ifndef TETRISFACTORY_H
+#define TETRISFACTORY_H
+
 #include "Tetris.h"
 #include "Block.h"
 
 class TetrisFactory {
 public:
-	Block* MakeBlock(int y, int x);
+	Block* MakeBlock(int width);
 	Tetris* MakeTetris(int width, int height);
 };
 
-Block* TetrisFactory::MakeBlock(int y, int x) {
-	return new Block(y, x);
+Block* TetrisFactory::MakeBlock(int width) {
+	return new Block(0, width / 2 - 2);
 }
 
 Tetris* TetrisFactory::MakeTetris(int width, int height) {
 	return new Tetris(width + 2, height + 2);
 }
+
+#endif /* TETRISFACTORY_H */
