9e32b3e52a1d9374ffaae1011d833a103c00f9bc
uso529133 <uso529133@gmail.com>
2018-09-25 15:36:37 +0900

    9/25 #5

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,27 @@
 
 #include "Tetris.h"
+#include "TetrisFactory.h"
 
 const int width = 15;
 const int height = 20;
 	
 int main() {
-	Tetris game(width, height);
-	Block* aBlock = new Block(1, width / 2 - 2);
+
+	TetrisFactory factory;
+	
+	Tetris* game = factory.MakeTetris(width, height);
+	Block* block = factory.MakeBlock(1, width / 2 - 2);
 	
 	while (true) {
-		aBlock->MoveBy(1, 0);
-		if (game.isDuplicateWith(aBlock)) {
-			game.ApplyBlock(aBlock);
-			aBlock = new Block(1, width / 2 - 2);
+		block->MoveBy(1, 0);
+		if (game->isDuplicateWith(block)) {
+			block->MoveBy(-1, 0);
+			game->ApplyBlock(block);
+			block = factory.MakeBlock(1, width / 2 - 2);
 		}
 
-		game.RefreshBuffer(aBlock);
-		game.PrintBuffer();
+		game->RefreshBuffer(block);
+		game->PrintBuffer();
 		
 		_sleep(100);
 	}
--- TetrisFactory.h
+++ TetrisFactory.h
@@ -1,4 +1,17 @@
 
+#include "Tetris.h"
+#include "Block.h"
+
 class TetrisFactory {
-	
+public:
+	Block* MakeBlock(int y, int x);
+	Tetris* MakeTetris(int width, int height);
 };
+
+Block* TetrisFactory::MakeBlock(int y, int x) {
+	return new Block(y, x);
+}
+
+Tetris* TetrisFactory::MakeTetris(int width, int height) {
+	return new Tetris(width + 2, height + 2);
+}
